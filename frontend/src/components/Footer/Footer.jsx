9aaba8fd161ce6285ee1b0b3445e3b4ef025bece
msaitag <88306395+msaitag@users.noreply.github.com>
2024-07-13 20:07:11 +0200

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <div className='footer' id='footer'>
       <div className="footer-content">
         <div className="footer-content-left">
-            <img src={assets.logo} alt="" />
+            <img className="logo" src={assets.logo} alt="" />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel et similique totam exercitationem atque eum!</p>
             <div className="footer-social-icons">
                 <img src={assets.facebook_icon} alt="" />
