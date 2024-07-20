571635c6445b2ea6c9cd317f1f8880c031549ed7
msaitag <88306395+msaitag@users.noreply.github.com>
2024-07-20 20:32:41 +0200

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className='header'>
         <div className="header-contents">
             <h2>Order your favourite food here</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam ducimus ullam nam nulla dolores eligendi?</p>
+            <p>We have the best chefs of their kind. Have you tried our delicious plates yet?</p>
             <button>View Menu</button>
         </div>
     </div>
